b6bc4140bdf62d5660c52fc24d99fb99cc0bd4d4
Manuel Gutierrez <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-10-07 12:08:29 -0500

    Initialize repo and add material-ui

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default App;
