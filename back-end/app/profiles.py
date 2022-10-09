ebb3c12836ae0cf3e7e6afbbe488adf8f7495517
ManuelG28 <36460775+ManuelG28@users.noreply.github.com>
2022-10-09 16:04:06 -0500

    Add cors (#5)

--- back-end/app/profiles.py
+++ back-end/app/profiles.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.data import make_profiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+profiles = make_profiles()
 
 
 @app.get("/", status_code=200)
@@ -11,10 +22,10 @@ async def get_profiles():
 
 @app.get("/profiles", status_code=200)
 async def get_profiles():
-    return make_profiles()
+    return profiles
 
 
 @app.get("/profiles/{profile_id}", status_code=200)
 async def get_profile(profile_id: int):
     return list(filter(lambda profile: profile.id ==
-                       profile_id, make_profiles()))
+                       profile_id, profiles))
